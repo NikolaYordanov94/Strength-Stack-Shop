9572bba994f382a1331c4865f860e931fe06263e
nikola_bg94 <nikola_bg94@abv.bg>
2023-11-29 22:20:00 +0200

    Initializing controller for comments

---
+++ src/main/java/bg/softuni/strengthstackshop/controller/CommentController.java
@@ -0,0 +1,9 @@
+package bg.softuni.strengthstackshop.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CommentController {
+    //todo logic for comment templates
+
+}
