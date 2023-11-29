3b080cf987e120305075ea0827b320a7a8fea945
nikola_bg94 <nikola_bg94@abv.bg>
2023-11-29 22:02:38 +0200

    Adding comment repository

---
+++ src/main/java/bg/softuni/strengthstackshop/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package bg.softuni.strengthstackshop.repository;
+
+import bg.softuni.strengthstackshop.model.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+
+}
